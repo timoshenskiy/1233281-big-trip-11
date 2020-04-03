5becf56b8f5b5b64e9cb701b1710fc483eb0d0ee
Alexandr Timoshenskiy <timoshenskiy.ad@gmail.com>
2020-04-03 11:57:40 +0300

    Личный проект: время разбивать камни

---
+++ src/components/travel-cost.js
@@ -0,0 +1,9 @@
+const createTravelCostTemplate = () => {
+  return (
+    `<p class="trip-info__cost">
+              Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+    </p>`
+  );
+};
+
+export {createTravelCostTemplate};
---
+++ src/components/travel-point-list.js
@@ -0,0 +1,14 @@
+const createTravelPointListTemplate = () => {
+  return (
+    `<ul class="trip-days">
+            <li class="trip-days__item  day">
+              <div class="day__info"></div>
+
+              <ul class="trip-events__list">
+              </ul>
+            </li>
+          </ul>`
+  );
+};
+
+export {createTravelPointListTemplate};
