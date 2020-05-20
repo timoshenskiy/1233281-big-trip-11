82ffa582ae2c481c5e54217393e771fe1e8bdd24
Alexandr Timoshenskiy <timoshenskiy.ad@gmail.com>
2020-05-20 20:51:17 +0300

    Уберет неиспользуемые части кода Б18

--- src/const.js
+++ src/const.js
@@ -1,7 +1,5 @@
 const POINT_TYPES_TRANSFER = [`taxi`, `bus`, `train`, `ship`, `transport`, `drive`, `flight`];
 const POINT_TYPES_ACTIVITY = [`check-in`, `sightseeing`, `restaurant`];
-const POINT_OFFERS_TITLES = [`Rent a car`, `Add luggage`, `Switch to comfort class`, `Add Meal`, `Choose seats`, `Travel by train`];
-const POINT_DESTINATIONS = [`Moscow`, `Kazan`, `Zelenograd`, `Vladivostok`, `Tver`, `London`, `New-York`, `Paris`];
 const MONTH_NAMES = [
   `Jan`,
   `Feb`,
@@ -22,4 +20,4 @@ const FilterType = {
   PAST: `past`,
 };
 
-export {POINT_TYPES_TRANSFER, POINT_TYPES_ACTIVITY, POINT_OFFERS_TITLES, POINT_DESTINATIONS, MONTH_NAMES, FilterType};
+export {POINT_TYPES_TRANSFER, POINT_TYPES_ACTIVITY, MONTH_NAMES, FilterType};
